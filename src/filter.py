ffa8fbc3ac36fb2f9e690746b548463cfb1117f6
Aamir Husain <aamir300@gmail.com>
2019-09-03 17:51:04 -0400

    Add basic node to echo incoming twist topic

---
+++ src/filter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+
+class TwistFilter:
+    def __init__(self):
+        # Set up publishers/subscribers
+        self.cmd_in = rospy.Subscriber('cmd_in', Twist, self.callback)
+
+        rospy.loginfo('Filter ready!')
+
+    def callback(self, data):
+        rospy.loginfo(data)
+
+def main():
+    try:
+        rospy.init_node('twist_filter')
+        filter = TwistFilter()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        rospy.loginfo('Shutting down...')
+
+if __name__ == '__main__':
+    main()

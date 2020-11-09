3201c0b0482b6f0669b59fbd99bf5108ca073534
Aamir Husain <aamir.husain@verizonwireless.com>
2020-11-09 16:54:57 -0500

    use new architecture. set a default config for active filters so we aren't over processing

--- src/init_fir.py
+++ src/init_fir.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python
 import rospy
-from component_filters import TwistFilterObject, FIRFilter
+from component_filters import FIRTwistFilterObject
 from filter_twist import TwistFilter
 
 def main():
     while not rospy.is_shutdown():
         rospy.init_node('fir_filter')
 
-        # Get size for sample array and check it is a valid size
-        num_samples = rospy.get_param('~num_samples')
-        if num_samples < 2:
-            raise Exception('ERROR - Filter sample size must be larger than 1')
+        # Get list of filters to activate, defaults to planar motion x/theta
+        active_filters = rospy.get_param('~active_filters', {
+            'linear': {
+                'x': True,
+                'y': False,
+                'z': False
+            },
+            'angular': {
+                'x': False,
+                'y': False,
+                'z': True
+            }
+        })
 
-        # Get list of weights and make sure they equal sample size
-        weights = rospy.get_param('~weights')
-        if len(weights) != num_samples:
-            raise Exception('ERROR - Number of weights must match sample size')
-        
-        # Create filters for each component of the incoming twist signal
-        components = TwistFilterObject()
-        components.linear.x = FIRFilter()
-        components.linear.y = FIRFilter()
-        components.linear.z = FIRFilter()
-        components.angular.x = FIRFilter()
-        components.angular.y = FIRFilter()
-        components.angular.z = FIRFilter()
+        # Create component filters
+        component_filters = FIRTwistFilterObject(active_filters)
 
         # Initialize the twist filter
-        twist_filter = TwistFilter(components)
+        twist_filter = TwistFilter(component_filters)
 
         rospy.spin()
 

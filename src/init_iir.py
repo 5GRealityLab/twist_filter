1a9dc9d6fabc8078ff9700394343f72ee925ac1d
Aamir Husain <aamir.husain@verizonwireless.com>
2020-11-09 19:40:48 -0500

    update to use dynammic reconfigure and new twist filter class

--- src/init_iir.py
+++ src/init_iir.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python
 import rospy
-from component_filters import TwistFilterObject, IIRFilter
+from component_filters import IIRTwistFilterObject
 from filter_twist import TwistFilter
 
 def main():
     while not rospy.is_shutdown():
         rospy.init_node('iir_filter')
 
-        # Get size for sample array and check it is a valid size
-        in_samp_size = rospy.get_param('~num_samples')
-        out_samp_size = rospy.get_param('~num_out_samples')
-        if in_samp_size < 1 or out_samp_size < 1:
-            raise Exception('ERROR - Filter sample size must be larger than 0')
-
-        # Get list of weights and make sure they equal thier respective sample sizes
-        in_weights = rospy.get_param('~weights')
-        out_weights = rospy.get_param('~out_weights')
-        if len(in_weights) != in_samp_size or len(out_weights) != out_samp_size:
-            raise Exception('ERROR - Number of weights must match sample size')
+        # Get list of filters to activate, defaults to planar motion x/theta
+        active_filters = rospy.get_param('~active_filters', {
+            'linear': {
+                'x': True,
+                'y': False,
+                'z': False
+            },
+            'angular': {
+                'x': False,
+                'y': False,
+                'z': True
+            }
+        })
         
-        # Create filters for each component of the incoming twist signal
-        components = TwistFilterObject()
-        components.linear.x = IIRFilter()
-        components.linear.y = IIRFilter()
-        components.linear.z = IIRFilter()
-        components.angular.x = IIRFilter()
-        components.angular.y = IIRFilter()
-        components.angular.z = IIRFilter()
+        # Create component filters
+        component_filters = IIRTwistFilterObject(active_filters)
 
         # Initialize the twist filter
-        twist_filter = TwistFilter(components)
+        twist_filter = TwistFilter(component_filters)
 
         rospy.spin()
 
